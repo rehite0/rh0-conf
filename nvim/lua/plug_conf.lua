ef41d43a460df6e64217d0a50992bb29b1ee9029
rh0 <125304115+rehite1@users.noreply.github.com>
2025-02-14 19:41:59 +0530

    small changes

--- nvim/lua/plug_conf.lua
+++ nvim/lua/plug_conf.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n","<leader>mir","<cmd>CellularAutomaton make_it_rain<CR>")
 vim.keymap.set("n","<leader>gol","<cmd>CellularAutomaton game_of_life<CR>")
+vim.api.nvim_create_user_command("MIR","CellularAutomaton make_it_rain",{desc="run animation"})
+vim.api.nvim_create_user_command("GOL","CellularAutomaton game_of_life",{desc="run animation"})
