4dff91a58a4db20f48009a578e760659a7a6c726
rh0 <125304115+rehite1@users.noreply.github.com>
2025-05-31 23:50:59 +0530

    small changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,29 +29,30 @@ vim.api.nvim_create_user_command("Elsp","popup lsp",{desc="launch lsp menu"})
 
 --options check :h options
 local opt={
-	clipboard	= 'unnamedplus'
-	--,tabstop	= 4
-	,number		= true
-	,relativenumber	= false
-	,numberwidth	= 3
-	,hlsearch	= false
-	,shiftwidth	= 4
-	,wrap		= true
-	,ignorecase	= true
-	,smartcase	= true
-	,termguicolors	= true
-	,backup		= false
-	,swapfile	= false
-	,mouse		= "a"
-	,autoread	= true
-	,autoindent	= true
-	,smartindent	= true
-	,encoding	= "utf-8"
-	,scrolloff	= 2
-	,nrformats	= 'hex'
-	,backspace	= "indent,eol,start"
-	,history	= 1000
-	,wrapscan	= true
+    clipboard		= 'unnamedplus'
+    ,number		= true
+    ,relativenumber	= false
+    ,numberwidth	= 3
+    ,hlsearch		= false
+    ,wrap		= true
+    ,ignorecase		= true
+    ,smartcase		= true
+    ,termguicolors	= true
+    ,backup		= false
+    ,swapfile		= false
+    ,mouse		= "a"
+    ,autoread		= true
+    ,tabstop		= 4
+    ,noexpandtab	= true
+    ,shiftwidth		= 4
+    ,autoindent		= true
+    ,smartindent	= true
+    ,encoding		= "utf-8"
+    ,scrolloff		= 2
+    ,nrformats		= 'hex'
+    ,backspace		= "indent,eol,start"
+    ,history		= 1000
+    ,wrapscan		= true
 }
 vim.cmd([[
     aunmenu PopUp
