a7c36270457f628b76f43ade294bf646170d12a5
rh0 <125304115+rehite0@users.noreply.github.com>
2026-06-01 19:55:28 +0530

    added rofi and xdg user dirs

---
+++ .scripts/vivi.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_ex"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_rc"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_phy"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_art"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_co"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_puz"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_psy"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_p"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="rh_g"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="ap"
+hyprctl dispatch exec -- flatpak run com.vivaldi.Vivaldi --profile-directory="kas"
